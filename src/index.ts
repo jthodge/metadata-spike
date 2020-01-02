9c031943bfa2331e308413e5d4cd260fce2e21a7
Taylor Hodge <j.taylor.hodge@gmail.com>
2020-01-02 15:57:51 -0500

    init commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import { idText } from "typescript";
+// import shelljs from "shelljs";
+
+const app = express();
+
+app.use(express.urlencoded());
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    message: "Generate Federated XML Metadata",
+    title: "Metadata Spike"
+  });
+});
+
+app.post("/generate", (req: any, res: any) => {
+  const entityId = req.body.entityId;
+  // const entityIdWithoutDomain = `${entityId}`.replace(".com", "");
+
+  const shell = require("shelljs");
+  shell.exec(
+    `./generate-metadata.sh ${entityId} https://${entityId}/auth/adfs`
+  );
+
+  const file = `./${entityId}.xml`;
+  res.download(file);
+
+  // res.send(`https://workos.com/${entityIdWithoutDomain}/saml/metadata`);
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${PORT}`);
+});
